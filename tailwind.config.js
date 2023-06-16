c6fb1bebdc823de0cedeb76e48e33a5e2ba50a2c
Lttce <31960436+Lttce@users.noreply.github.com>
2023-06-16 19:07:17 +0000

    remove: tailwindcss

--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{ts,tsx}',
-	]
-}
