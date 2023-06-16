4b161f5f9b23628acbde0b5987d1abfd35a77fe4
Lttce <31960436+Lttce@users.noreply.github.com>
2023-06-16 18:42:49 +0000

    remove: theme

--- app/theme.tsx
+++
@@ -1,8 +0,0 @@
-import { extendTheme } from "@chakra-ui/react";
-
-const theme = extendTheme({
-    config: {
-        initialColorMode: localStorage.getItem('chakra-ui-color-mode') || 'dark',
-    },
-});
-export default theme;
